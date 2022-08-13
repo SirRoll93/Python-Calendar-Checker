defda820be12a7c32e66ac81514ef9aa1dd3b767
ASUBONTENG VINCENT <109989332+SirRoll93@users.noreply.github.com>
2022-08-13 15:42:48 +0000

    Add files via upload

---
+++ Greetings.py
@@ -0,0 +1,20 @@
+#def greet():
+#    print("Good day, how are you?")
+#    return greet()
+#greet()
+
+#def greet(name):
+    #print(f"Hello {name} how are you?")
+    #return greet("Debbie")
+#greet("Yaw")
+
+
+names = ("I Love you billion times")
+
+while ('love' != names):
+    print(names)
+
+
+#for n in names:
+    #if(n == "I love you billion times"):continue
+    #print(n)
